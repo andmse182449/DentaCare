d1ad8dc23328cbf5543339e755056f4009408bff
Minh An <andmse182449@fpt.edu.vn>
2024-05-17 14:10:37 +0700

    Delete Bao project

--- CodeClinicDental/build/web/js/popup.js
+++
@@ -1,9 +0,0 @@
-const showPopup = document.querySelector('.show-popup');
-const popupContainer = document.querySelector('.popup-container');
-const closeBtn = document.querySelector('.close-btn');
-showPopup.onclick = () => {
-    popupContainer.classList.add('active');
-}
-closeBtn.onclick = () => {
-    popupContainer.classList.remove('active');
-}
